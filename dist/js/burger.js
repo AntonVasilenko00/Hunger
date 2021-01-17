1fb29fe62c82b200f42b4287e40ae52b784b8d45
Anton <anton.vasilenko00@gmail.com>
2021-01-17 21:52:26 +0300

    feat: finish page

---
+++ dist/js/burger.js
@@ -0,0 +1,5 @@
+const burgerMenu = document.querySelector(".header__burger");
+console.log(burgerMenu);
+burgerMenu.addEventListener("click", () => {
+    burgerMenu.classList.toggle("header__burger--clicked")
+});
